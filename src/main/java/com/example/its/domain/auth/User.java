03d3e525dc5293ce2e8ac83979df146066898a4b
kyoto-kanko <kyoto.kanko.biz@gmail.com>
2023-09-21 23:43:49 +0900

    update: user model create

---
+++ src/main/java/com/example/its/domain/auth/User.java
@@ -0,0 +1,11 @@
+package com.example.its.domain.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+public class User {
+
+    private String username;
+    private String password;
+}
+
